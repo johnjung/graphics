484274e158b02d32f5580c296ffd457766edd74a
John Jung <jej@moss.lib.uchicago.edu>
2019-04-18 15:46:20 -0500

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+
+setup(
+    name='graphics',
+    description='Scripts to produce diagrams and graphics',
+    version='0.0.1',
+    long_description=readme(),
+    author='John Jung',
+    author_email='john@johnjung.us',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/johnjung/graphics',
+    scripts=[
+        'graphics/beer_flavor_wheel',
+        'graphics/two_by_two',
+        'graphics/venn',
+    ]
+)
